6cdb88a118204e3a7ae615a564a62d6fbf6599d0
Sam Belliveau <sam.belliveau@gmail.com>
2019-10-27 18:57:18 -0400

    updated to new version of stuylib

--- src/NetKeyServer.java
+++ src/NetKeyServer.java
@@ -1,5 +1,10 @@
 import java.awt.Frame;
-import edu.stuylib.input.keyboard.server.NetKeyWindow;
+import java.awt.*;
+
+import edu.stuylib.input.keyboard.computer.NetKeyWindow;
+import edu.stuylib.input.keyboard.NetKeyboardInfo;
+import edu.stuylib.network.NetworkTableClient;
+import edu.wpi.first.networktables.*;
 
 public class NetKeyServer {
     public static void main(String[] args) {
